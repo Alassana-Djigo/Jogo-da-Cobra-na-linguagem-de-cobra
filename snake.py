49e65e488b4c331e51cafa853c4474dd3a23a690
Alassana Djigo <alassana7djigo@gmail.com>
2022-08-05 09:08:45 +0100

    Carregado

---
+++ snake.py
@@ -0,0 +1,86 @@
+import curses
+from random import randint
+
+#constantes
+
+WINDOW_WIDTH = 60  # numero de coluna 
+WINDOW_HEIGHT = 20 # numero de linhas 
+'''
+Número de blocos na janela por linha = WINDOW_WIDTH -2.
+O índice do bloco x varia de 1 a WINDOW_WIDTH -2.
+Número de blocos na janela por coluna = WINDOW_HEIGHT -2.
+O índice do bloco y varia de 1 a WINDOW_HEIGHT -2.
+'''
+
+# setup window
+curses.initscr()
+win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0) # linhas, colunas
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1) # -1
+
+# comida da cobra
+snake = [(4, 4), (4, 3), (4, 2)]
+food = (6, 6)
+
+win.addch(food[0], food[1], '#')
+# logica do jogo
+score = 0
+
+ESC = 27
+key = curses.KEY_RIGHT
+
+while key != ESC:
+    win.addstr(0, 2, 'Score ' + str(score) + ' ')
+    win.timeout(150 - (len(snake)) // 5 + len(snake)//10 % 120) # add velocidade
+
+    prev_key = key
+    event = win.getch()
+    key = event if event != -1 else prev_key
+
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+        key = prev_key
+
+    # calcular a proxima cordenada
+    y = snake[0][0]
+    x = snake[0][1]
+    if key == curses.KEY_DOWN:
+        y += 1
+    if key == curses.KEY_UP:
+        y -= 1
+    if key == curses.KEY_LEFT:
+        x -= 1
+    if key == curses.KEY_RIGHT:
+        x += 1
+
+    snake.insert(0, (y, x)) # acrescentar O(n)
+
+    # checar se esta tocando nas bordas
+    if y == 0: break
+    if y == WINDOW_HEIGHT-1: break
+    if x == 0: break
+    if x == WINDOW_WIDTH -1: break
+
+    # se a cobra correr sobre si mesma
+    if snake[0] in snake[1:]: break
+
+    if snake[0] == food:
+        # comeu 
+        score += 1
+        food = ()
+        while food == ():
+            food = (randint(1,WINDOW_HEIGHT-2), randint(1,WINDOW_WIDTH -2))
+            if food in snake:
+                food = ()
+        win.addch(food[0], food[1], '#')
+    else:
+        # mover a cobra
+        last = snake.pop()
+        win.addch(last[0], last[1], ' ')
+
+    win.addch(snake[0][0], snake[0][1], '*')
+
+curses.endwin()
+print(f"Final score = {score}")
